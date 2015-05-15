93f2ca54d021378de35e71a18f87f06667a0a12c
Brandon Rich <brich@nd.edu>
2015-05-15 16:18:12 +0000

    use login.nd.edu/cas for development too

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,5 +39,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.rack_cas.server_url = 'https://login-test.cc.nd.edu/cas/'
+  config.rack_cas.server_url = 'https://login.nd.edu/cas/'
 end
