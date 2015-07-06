4f66a4a757bf1055067e9a2627ca06a7f01f5b87
Brandon Rich <brich@nd.edu>
2015-07-06 03:51:42 +0000

    use AWS STS (token service) for MFA.  assume role on ec2 instance

--- app/models/ec2/service.rb
+++ app/models/ec2/service.rb
@@ -1,7 +1,26 @@
 class Ec2::Service
 
 	def initialize
-		@@ec2 = Aws::EC2::Client.new
+		@@ec2 ||= ec2
+	end
+
+	def ec2
+
+		sts = Aws::STS::Client.new
+		if ENV.has_key?( 'AWS_MFA_TOKEN' )
+			puts "get session token"
+			session = sts.get_session_token( 
+				duration_seconds: 3600, 
+				serial_number: ENV['AWS_MFA_SERIAL'], 
+				token_code: ENV['AWS_MFA_TOKEN'] )
+		else
+			puts "assume role"
+			session = sts.assume_role( 
+				role_arn: ENV['ROLE_ARN'], 
+				role_session_name: 'foo' )
+		end
+		creds = Aws::Credentials.new( session.credentials.access_key_id, session.credentials.secret_access_key, session.credentials.session_token )
+		Aws::EC2::Client.new( credentials: creds )
 	end
 
 	def instances( state )
